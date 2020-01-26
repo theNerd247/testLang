3cfedb26e8aa09c6b81bead179a9d025bc35933a
theNerd247 <noah.harvey247 gm>
2020-01-26 18:52:34 -0500

    finished untyped evaluation

--- src/Eval.hs
+++ src/Eval.hs
@@ -3,12 +3,13 @@
 module Eval where
 
 import Syntax
+import Control.Monad (join)
 import qualified Data.Map as M
 
 data Value = 
     VInt Int
   | VBool Bool
-  | VClosure String Expr Scope
+  | VClosure Scope String Expr 
 
 instance Show Value where
   show (VInt x)   = show x
@@ -26,8 +27,15 @@ eval :: Scope -> Expr -> EvalM Value
 eval _ (Lit (LInt n))  = return $ VInt n
 eval _ (Lit (LBool b)) = return $ VBool b
 eval s (Var x)         = maybe (Left $ "not in scope: " ++ x) return (M.lookup x s)
-eval s (Lam x body)    = return $ VClosure x body s
-eval _ _               = Left "not implemented"
+eval s (Lam x body)    = return $ VClosure s x body
+eval s (App a b)       = join $ apply <$> (eval s a) <*> (eval s b)
+
+apply :: Value -> Value -> EvalM Value
+apply (VClosure s n t1) t2 = eval (extend s n t2) t1
+apply _ _ = Left "Not an appliable closure"
+
+extend :: Scope -> Name -> Value -> Scope
+extend s n v = M.insert n v s
 
 emptyScope :: Scope
 emptyScope = M.empty
