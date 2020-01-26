ed83379b90d0a201a406037ae1e0ecf5ad3ae48a
theNerd247 <noah.harvey247 gm>
2020-01-26 09:41:48 -0500

    finished basic eval function

--- src/Eval.hs
+++ src/Eval.hs
@@ -5,7 +5,12 @@ module Eval where
 import Syntax
 
 eval :: Expr -> Expr
-eval Tr = Tr
-eval Fl = Fl
-eval Zero = Zero
-eval Succ 
+eval Tr         = Tr
+eval Fl         = Fl
+eval Zero       = Zero
+eval (Succ e)   = Succ (eval e)
+eval (Pred e)   = Pred (eval e)
+eval (If i t e) =
+  case (eval i) of
+    Tr → eval t
+    _  → eval e
